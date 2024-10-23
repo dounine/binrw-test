702000db5aba0fda3abed18fa6ecc9f8537317ff
lake <102535481@qq.com>
2024-10-23 18:54:51 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::Cursor;
 use binrw::{BinRead, BinReaderExt, binrw};
 
 #[binrw]
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum MachType {
     #[br(magic = 0xcafebabeu32)]
     FatMaGic,
@@ -20,17 +20,36 @@ enum MachType {
     Unknown(u32),
 }
 
+pub type CpuType = i32;
+pub type CpuSubtype = i32;
+
 #[binrw]
 #[derive(Debug)]
-struct Macho {
-    magic: MachType,
+pub struct MachHeader {
+    pub magic: MachType,
+    /* mach magic 标识符，用来确定其属于 64位 还是 32位 */
+    pub cpu_type: CpuType,
+    /* CPU 类型标识符，同通用二进制格式中的定义 */
+    pub cpu_subtype: CpuSubtype,
+    /* CPU 子类型标识符，同通用二级制格式中的定义 */
+    pub file_type: u32,
+    /* 文件类型 */
+    pub n_cmds: u32,
+    /* 加载器中加载命令(Load commands)的数量 */
+    pub size_of_cmds: u32,
+    /* 加载器中加载命令的总字节大小 */
+    pub flags: u32,
+    /* dyld 的标志，主要与系统的加载、链接有关 */
+    #[br(if (magic == MachType::MachoMagic || magic == MachType::MachoMagic64))]
+    pub reserved: u32,
+    /* 64 位的保留字段 */
 }
 
 
 fn main() {
     let data = fs::read("./data/ios").unwrap();
     let mut reader = Cursor::new(data);
-    let macho = Macho::read_ne(&mut reader).unwrap();
+    let macho = MachHeader::read_ne(&mut reader).unwrap();
 
     println!("{:?}", macho);
 }
