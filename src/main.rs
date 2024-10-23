c2a472f743728dde877f53325195de22726ef964
lake <102535481@qq.com>
2024-10-23 23:10:59 +0800

    aa

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ use std::ptr::read;
 #[binrw]
 #[derive(Debug, PartialEq)]
 enum MachType {
-    #[brw(magic = 0xcafebabeu32)]
+    #[brw(magic(0xcafebabeu32))]
     FatMaGic,
-    #[brw(magic = 0xbebafecau32)]
+    #[brw(magic(0xbebafecau32))]
     FatCiGam,
-    #[brw(magic = 0xfeedfaceu32)]
+    #[brw(magic(0xfeedfaceu32))]
     MachoMagic,
-    #[brw(magic = 0xfeedfacfu32)]
+    #[brw(magic(0xfeedfacfu32))]
     MachoMagic64,
-    #[brw(magic = 0xcefaedfeu32)]
+    #[brw(magic(0xcefaedfeu32))]
     MachoCiGam,
-    #[brw(magic = 0xcffaedfeu32)]
+    #[brw(magic(0xcffaedfeu32))]
     MachoCiGam64,
     Unknown(u32),
 }
@@ -24,27 +24,27 @@ enum MachType {
 #[binrw]
 #[derive(Debug, PartialEq)]
 enum FileType {
-    #[brw(magic = 0x1u32)]
+    #[brw(magic(0x1u32))]
     MachObject,
-    #[brw(magic = 0x2u32)]
+    #[brw(magic(0x2u32))]
     MachExecute,
-    #[brw(magic = 0x3u32)]
+    #[brw(magic(0x3u32))]
     MachFVMLib,
-    #[brw(magic = 0x4u32)]
+    #[brw(magic(0x4u32))]
     MachCore,
-    #[brw(magic = 0x5u32)]
+    #[brw(magic(0x5u32))]
     MachPreload,
-    #[brw(magic = 0x6u32)]
+    #[brw(magic(0x6u32))]
     MachDyLib,
-    #[brw(magic = 0x7u32)]
+    #[brw(magic(0x7u32))]
     MachDyLinker,
-    #[brw(magic = 0x8u32)]
+    #[brw(magic(0x8u32))]
     MachBundle,
-    #[brw(magic = 0x9u32)]
+    #[brw(magic(0x9u32))]
     MachDyLibStub,
-    #[brw(magic = 0xau32)]
+    #[brw(magic(0xau32))]
     MachDsym,
-    #[brw(magic = 0xbu32)]
+    #[brw(magic(0xbu32))]
     MachKextBundle,
     Unknown(u32),
 }
@@ -55,7 +55,7 @@ pub type CpuSubtype = i32;
 #[binrw]
 #[derive(Debug)]
 pub struct MachHeader {
-    magic: MachType, // mach magic 标识符，用来确定其属于 64位 还是 32位
+    magic: MachType,         // mach magic 标识符，用来确定其属于 64位 还是 32位
     cpu_type: CpuType,       //CPU 类型标识符，同通用二进制格式中的定义
     cpu_subtype: CpuSubtype, //CPU 子类型标识符，同通用二级制格式中的定义
     file_type: FileType,     //文件类型，主要用来区分可执行文件、动态库、静态库等
