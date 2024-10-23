bef47b3faf45dce15e5320c01c07d93278a86f65
lake <102535481@qq.com>
2024-10-23 19:01:26 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -20,29 +20,50 @@ enum MachType {
     Unknown(u32),
 }
 
+#[binrw]
+#[derive(Debug, PartialEq)]
+enum FileType {
+    #[br(magic = 0x1u32)]
+    MachObject,
+    #[br(magic = 0x2u32)]
+    MachExecute,
+    #[br(magic = 0x3u32)]
+    MachFVMLib,
+    #[br(magic = 0x4u32)]
+    MachCore,
+    #[br(magic = 0x5u32)]
+    MachPreload,
+    #[br(magic = 0x6u32)]
+    MachDyLib,
+    #[br(magic = 0x7u32)]
+    MachDyLinker,
+    #[br(magic = 0x8u32)]
+    MachBundle,
+    #[br(magic = 0x9u32)]
+    MachDyLibStub,
+    #[br(magic = 0xau32)]
+    MachDsym,
+    #[br(magic = 0xbu32)]
+    MachKextBundle,
+    Unknown(u32),
+}
+
 pub type CpuType = i32;
 pub type CpuSubtype = i32;
+
 
 #[binrw]
 #[derive(Debug)]
 pub struct MachHeader {
-    pub magic: MachType,
-    /* mach magic 标识符，用来确定其属于 64位 还是 32位 */
-    pub cpu_type: CpuType,
-    /* CPU 类型标识符，同通用二进制格式中的定义 */
-    pub cpu_subtype: CpuSubtype,
-    /* CPU 子类型标识符，同通用二级制格式中的定义 */
-    pub file_type: u32,
-    /* 文件类型 */
-    pub n_cmds: u32,
-    /* 加载器中加载命令(Load commands)的数量 */
-    pub size_of_cmds: u32,
-    /* 加载器中加载命令的总字节大小 */
-    pub flags: u32,
-    /* dyld 的标志，主要与系统的加载、链接有关 */
+    pub magic: MachType,// mach magic 标识符，用来确定其属于 64位 还是 32位
+    pub cpu_type: CpuType,//CPU 类型标识符，同通用二进制格式中的定义
+    pub cpu_subtype: CpuSubtype,//CPU 子类型标识符，同通用二级制格式中的定义
+    pub file_type: FileType,//文件类型，主要用来区分可执行文件、动态库、静态库等
+    pub n_cmds: u32,//加载器中加载命令(Load commands)的数量
+    pub size_of_cmds: u32,//加载器中加载命令的总字节大小
+    pub flags: u32, // 标志位，主要用来表示是否是64位的二进制文件，是否是可执行文件等
     #[br(if (magic == MachType::MachoMagic || magic == MachType::MachoMagic64))]
-    pub reserved: u32,
-    /* 64 位的保留字段 */
+    pub reserved: u32,// 64 位的保留字段
 }
 
 
