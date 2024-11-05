1e57213c7edec06c8af1c6ae25d7c6b4d5f792d9
lake <102535481@qq.com>
2024-11-05 14:45:04 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use binrw::{binrw, BinRead, BinReaderExt, BinResult, BinWrite, NullString};
 use std::fs;
 use std::fs::File;
 use std::io::{Cursor, Read, SeekFrom};
+use std::ops::Neg;
 
-const CPU_TYPE_X86_64: i32 = 7 | 0x01000000;
-#[binrw(repr(i32))]
+#[binrw]
+#[brw(is_big = big_endian,import{big_endian: bool})]
 #[derive(Debug, PartialEq, Eq)]
 enum CpuType {
     #[brw(magic = 7_i32)]
     X86,
-    #[brw(magic = 0x01000000_i32)]
+    #[brw(magic = 16777223_i32)]
     X86_64,
     #[brw(magic = 12_i32)]
     Arm,
@@ -104,69 +105,142 @@ enum CmdType {
     LcCodeSignature,
     Unknown(u32),
 }
-
 #[binrw]
 #[derive(Debug)]
-struct LoadCommand {
-    cmd: CmdType,
-    #[bw(calc = (info.data.len() + 8) as u32)]
-    cmd_size: u32,
-    #[br(args{ cmd:cmd.clone(),cmd_size:cmd_size -8 })]
-    info: CommandInfo,
+struct Section<T: for<'a> BinRead<Args<'a> = ()> + for<'a> BinWrite<Args<'a> = ()>> {
+    section_name: [u8; 16], /* name of this section */
+    segment_name: [u8; 16], /* segment this section goes in */
+    addr: T,                /* memory address of this section */
+    size: T,                /* size in bytes of this section */
+    offset: u32,            /* file offset of this section */
+    align: u32,             /* section alignment (power of 2) */
+    rel_offset: u32,        /* file offset of relocation entries */
+    nreloc: u32,            /* number of relocation entries */
+    flags: u32,             /* flags (section type and attributes)*/
+    reserved1: u32,         /* reserved */
+    reserved2: u32,         /* reserved */
+    reserved3: Option<u32>,
+}
+#[binrw]
+#[derive(Debug)]
+#[brw(little)]
+struct Data {
+    version: u16,
+    #[br(temp)]
+    #[bw(calc = items.len().try_into().unwrap())]
+    num_items: u32,
+    #[br(args{ count: 10, inner: (true,) })]
+    items: Vec<Item>,
 }
 
 #[binrw]
-#[br(import{ cmd:CmdType,cmd_size:u32 })]
+#[derive(Debug)]
+#[brw(little)]
+#[br(import(version: bool))]
+struct Item {
+    field1: u32,
+    #[br(if(version, 0))]
+    field2: u64,
+}
+#[binrw]
+#[derive(Debug)]
+enum LoadCommand {
+    #[brw(magic = 0x00000001_u32)]
+    Segment {
+        cmd_size: u32,
+        segment_name: [u8; 16], /* segment name */
+        vm_addr: u32,           /* memory address of this segment */
+        vm_size: u32,           /* memory size of this segment */
+        file_offset: u32,       /* file offset of this segment */
+        file_size: u32,         /* amount to map from the file */
+        max_prot: i32,          /* maximum VM protection */
+        init_prot: i32,         /* initial VM protection */
+        #[br(temp)]
+        #[bw(calc = sections.len().try_into().unwrap())]
+        nsects: u32, /* number of sections in segment */
+        flags: u32,             /* flags */
+        #[br(count = nsects)]
+        sections: Vec<Section<u32>>,
+    },
+    #[brw(magic = 0x00000019_u32)]
+    Segment64 {
+        cmd_size: u32,
+        segment_name: [u8; 16],
+        vm_addr: u64,
+        vm_size: u64,
+        file_offset: u64,
+        file_size: u64,
+        max_prot: i32,
+        init_prot: i32,
+        #[br(temp)]
+        #[bw(calc = sections.len().try_into().unwrap())]
+        nsects: u32,
+        flags: u32,
+        #[br(count = nsects)]
+        sections: Vec<Section<u64>>,
+    },
+    #[brw(magic = 0x00000021_u32)]
+    EncryptionInfo {
+        cmd_size: u32,
+        file_offset: u32,
+        file_size: u32,
+    },
+    #[brw(magic = 0x0000002c_u32)]
+    EncryptionInfo64 {
+        cmd_size: u32,
+        file_offset: u32,
+        file_size: u32,
+        crypt_id: u32,
+        padding: u32,
+    },
+    #[brw(magic = 0x0000001d_u32)]
+    CodeSignature {
+        cmd_size: u32,
+        file_offset: u32,
+        file_size: u32,
+    },
+    Unknown {
+        cmd: CmdType,
+        #[bw(calc = (data.len() + 8) as u32)]
+        cmd_size: u32,
+        #[br(count = cmd_size - 8)]
+        data: Vec<u8>,
+    },
+}
+
+#[binrw]
+#[br(import{ cmd_size:u32 })]
 #[derive(Debug, Default)]
 struct CommandInfo {
     #[br(count = cmd_size )]
     data: Vec<u8>,
 }
-// impl CommandInfo {
-//     #[binrw::parser(reader)]
-//     fn parse(shared: Vec<u8>) -> BinResult<[u8; 16]> {
-//         Ok([0u8; 16])
-//     }
-// }
-// impl CommandInfo {
-//     fn from_bytes(data: Vec<u8>) -> Self {
-//         Self {
-//             segment_name: [0u8; 16],
-//         }
-//     }
-// }
-
-// impl LoadCommand {
-//     pub fn parse<R: Read>(reader: &mut R) -> binrw::BinResult<Self> {
-//
-//     }
-// }
 
 #[binrw]
 #[derive(Debug)]
 pub struct MachHeader {
     magic: MachType, // mach magic 标识符，用来确定其属于 64位 还是 32位
-    #[br(seek_before = SeekFrom::Current(-4),map = |magic:MachType| (magic == MachType::MachoCiGam || magic == MachType::MachoCiGam64))]
+    #[br(seek_before = SeekFrom::Current(-4),map = |magic:MachType| matches!(magic,MachType::MachoCiGam|MachType::MachoCiGam64))]
     #[bw(ignore)]
     big_endian: bool,
-    #[br(seek_before = SeekFrom::Current(-4),map = |magic:MachType| (magic == MachType::MachoMagic64 || magic == MachType::MachoCiGam64))]
+    #[br(seek_before = SeekFrom::Current(-4),map = |magic:MachType| matches!(magic,MachType::MachoMagic64|MachType::MachoCiGam64))]
     #[bw(ignore)]
     bit_64: bool, //是否是64位的二进制文件
 
-    cpu_type: CpuType,       //CPU 类型标识符，同通用二进制格式中的定义
+    #[brw( args { big_endian: big_endian.clone() } )]
+    cpu_type: CpuType, //CPU 类型标识符，同通用二进制格式中的定义
     cpu_subtype: CpuSubtype, //CPU 子类型标识符，同通用二级制格式中的定义
     file_type: FileType,     //文件类型，主要用来区分可执行文件、动态库、静态库等
-    //magic 等于 CIGMA则使用小端
     #[br(is_big = big_endian )]
-    n_cmds: u32, //加载器中加载命令(Load commands)的数量
-    #[br(is_big =big_endian )]
+    count_of_cmds: u32, //加载器中加载命令(Load commands)的数量
+    #[br(is_big = big_endian )]
     size_of_cmds: u32, //加载器中加载命令的总字节大小
-    flags: u32, // 标志位，主要用来表示是否是64位的二进制文件，是否是可执行文件等
-    #[br(if(bit_64))]
-    #[bw(if(*bit_64))]
+    flags: u32,              // 标志位，主要用来表示是否是64位的二进制文件，是否是可执行文件等
+    // #[brw(if(bit_64.clone()))]
+    #[br(try)]
     reserved: Option<u32>, // 64 位的保留字段
 
-    #[br(count = n_cmds)]
+    #[br(count = count_of_cmds)]
     commands: Vec<LoadCommand>,
 }
 
@@ -256,5 +330,6 @@ fn main() {
     let mut reader = Cursor::new(&data);
     let macho = MachHeader::read_ne(&mut reader).unwrap();
     println!("2 {:?}", macho);
-    println!("length {:?}", data.len());
+    // println!("{}",CPU_TYPE_X86_64);
+    // println!("length {:?}", data.len());
 }
